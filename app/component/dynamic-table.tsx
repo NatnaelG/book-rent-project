bb3c9a212b7fdcb9510d934185f37f7760b146f8
NatnaelG <natigirma02@gmail.com>
2024-08-16 11:48:16 +0300

    ui change in the material react table

--- app/component/dynamic-table.tsx
+++ app/component/dynamic-table.tsx
@@ -4,7 +4,15 @@ import {
   useMaterialReactTable,
   type MRT_ColumnDef,
 } from "material-react-table";
-import { Typography } from "@mui/material";
+import {
+  Chip,
+  FormControlLabel,
+  Stack,
+  Switch,
+  Typography,
+} from "@mui/material";
+
+import CheckIcon from "@mui/icons-material/Check";
 
 //example data type
 type Person = {
@@ -13,9 +21,8 @@ type Person = {
   name: string;
   category: string;
   bookName: string;
-  status: string;
+  status: "ACTIVE" | "INACTIVE";
 };
-
 
 //nested data is ok, see accessorKeys in ColumnDef below
 const data: Person[] = [
@@ -25,103 +32,103 @@ const data: Person[] = [
     name: "Nardos T",
     category: "Fiction",
     bookName: "Derto Gada",
-    status: "Kentucky",
+    status: "INACTIVE",
   },
   {
-    number: 1,
+    number: 2,
     author: "Harry",
     name: "Nardos T",
     category: "Fiction",
     bookName: "Derto Gada",
-    status: "Kentucky",
+    status: "ACTIVE",
   },
   {
-    number: 1,
+    number: 3,
     author: "Harry",
     name: "Nardos T",
     category: "Fiction",
     bookName: "Derto Gada",
-    status: "Kentucky",
+    status: "ACTIVE",
   },
   {
-    number: 1,
+    number: 4,
     author: "Harry",
     name: "Nardos T",
     category: "Fiction",
     bookName: "Derto Gada",
-    status: "Kentucky",
+    status: "ACTIVE",
   },
   {
-    number: 1,
+    number: 5,
     author: "Harry",
     name: "Nardos T",
     category: "Fiction",
     bookName: "Derto Gada",
-    status: "Kentucky",
+    status: "ACTIVE",
   },
   {
-    number: 1,
+    number: 6,
     author: "Harry",
     name: "Nardos T",
     category: "Fiction",
     bookName: "Derto Gada",
-    status: "Kentucky",
+    status: "ACTIVE",
   },
   {
-    number: 1,
+    number: 7,
     author: "Harry",
     name: "Nardos T",
     category: "Fiction",
     bookName: "Derto Gada",
-    status: "Kentucky",
+    status: "ACTIVE",
   },
   {
-    number: 1,
+    number: 8,
     author: "Harry",
     name: "Nardos T",
     category: "Fiction",
     bookName: "Derto Gada",
-    status: "Kentucky",
+    status: "ACTIVE",
   },
   {
-    number: 1,
+    number: 9,
     author: "Harry",
     name: "Nardos T",
     category: "Fiction",
     bookName: "Derto Gada",
-    status: "Kentucky",
+    status: "ACTIVE",
   },
   {
-    number: 1,
+    number: 10,
     author: "Harry",
     name: "Nardos T",
     category: "Fiction",
     bookName: "Derto Gada",
-    status: "Kentucky",
+    status: "ACTIVE",
   },
   {
-    number: 1,
+    number: 11,
     author: "Harry",
     name: "Nardos T",
     category: "Fiction",
     bookName: "Derto Gada",
-    status: "Kentucky",
+    status: "ACTIVE",
   },
   {
-    number: 1,
+    number: 12,
     author: "Harry",
     name: "Nardos T",
     category: "Fiction",
     bookName: "Derto Gada",
-    status: "Kentucky",
+    status: "ACTIVE",
   },
   {
-    number: 1,
+    number: 13,
     author: "Harry",
     name: "Nardos T",
     category: "Fiction",
     bookName: "Derto Gada",
-    status: "Kentucky",
+    status: "ACTIVE",
   },
 ];
 
@@ -158,6 +165,57 @@ const DynamicTable = () => {
         accessorKey: "status",
         header: "Status",
         size: 150,
+        Cell: ({ renderedCellValue, row }) => (
+          <FormControlLabel
+            value="start"
+            sx={{ background: renderedCellValue === "ACTIVE" ? "#0080001A" : "#8000001A", borderRadius: "15px", p: 1 }}
+            control={
+              <Switch
+                color={renderedCellValue === "ACTIVE" ? "success" : "error"}
+                checked={renderedCellValue === "ACTIVE"}
+                name={`status-${row.original.number}`}
+                size="small"
+                sx={{
+                  width: "75px",
+                  "& .MuiButtonBase-root": {
+                    "& .MuiSwitch-thumb": {
+                      ml: renderedCellValue === "ACTIVE" ? "35px" : 0,
+                    },
+                  }
+                }}
+              />
+            }
+            label={
+              <Stack direction={"row"} spacing={1}>
+                <CheckIcon
+                  fontSize="small"
+                  color={renderedCellValue === "ACTIVE" ? "success" : "error"}
+                />
+                <Typography
+                  sx={{
+                    color:
+                      renderedCellValue === "ACTIVE" ? "#2e7d32" : "#d32f2f",
+                    textTransform: "capitalize",
+                  }}
+                  variant="subtitle2"
+                >
+                  {(renderedCellValue + "")?.toLowerCase()}
+                </Typography>
+              </Stack>
+            }
+            labelPlacement="start"
+          />
+          // <Chip
+          //   icon={
+          //     <CheckIcon
+          //       color={renderedCellValue === "ACTIVE" ? "success" : "error"}
+          //     />
+          //   }
+          //   label={renderedCellValue}
+          //   color={renderedCellValue === "ACTIVE" ? "success" : "error"}
+          //   variant="outlined"
+          // />
+        ),
       },
     ],
     []
